b79a49cc2a65e8ec172da7d08d4c5ef1c07767d0
FeiraTecnologicaMCM <appfeira3c2024@gmail.com>
2024-09-26 11:36:47 -0300

    tela creditos

---
+++ app/public/src/creditos.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+
+        <!-- HEAD -->
+        <?php
+            include('../assets/components/head.php')
+        ?>
+        <!-- FIM HEAD -->
+
+        <!-- !AQUI VAI O CSS DA SUA PÁGINA! -->
+        <link rel="stylesheet" href="../assets/css/creditos.css">
+    </head>
+
+    <body>
+
+        <!-- HEADER-->
+         <?php
+            include('../assets/components/header.php')
+         ?>
+
+         <h1 class="titulo_pagina" >Créditos aos desenvolvedores do site!</h1>
+        <!-- FIM HEADER-->
+
+        <!-- CONTEÚDO -->
+        <div class="conteudo">
+            <div class="list">
+                <h2 class="tit" >Protótipo</h2>
+                <div class="cols">
+                    <ul>
+                        <li> Danilo Keithi 3ºC</li>
+                        <li>Danilo Correia 3ºC</li>
+                        <li>David Santos 3ºC</li>
+                        <li>Enzo Siqueira 3ºC</li>
+                        <li>Gabriel Dantas 3ºC</li>
+                        <li>Gabriel Garcia 3ºC</li>
+                        <li>Nicolas Garcia 3ºC</li>
+                    </ul>
+                    <ul>
+                        <li> Danilo Keithi 3ºC</li>
+                        <li>Danilo Correia 3ºC</li>
+                        <li>David Santos 3ºC</li>
+                        <li>Enzo Siqueira 3ºC</li>
+                        <li>Gabriel Dantas 3ºC</li>
+                        <li>Gabriel Garcia 3ºC</li>
+                        <li>Nicolas Garcia 3ºC</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+            <div class="list">
+                <h2 class="tit" >FrontEnd</h2>
+                <div class="cols">
+                    <ul>
+                        <li> Danilo Keithi 3ºC</li>
+                        <li>Danilo Correia 3ºC</li>
+                        <li>David Santos 3ºC</li>
+                        <li>Enzo Siqueira 3ºC</li>
+                        <li>Gabriel Dantas 3ºC</li>
+                        <li>Gabriel Garcia 3ºC</li>
+                        <li>Nicolas Garcia 3ºC</li>
+                    </ul>
+                    <ul>
+                        <li> Danilo Keithi 3ºC</li>
+                        <li>Danilo Correia 3ºC</li>
+                        <li>David Santos 3ºC</li>
+                        <li>Enzo Siqueira 3ºC</li>
+                        <li>Gabriel Dantas 3ºC</li>
+                        <li>Gabriel Garcia 3ºC</li>
+                        <li>Nicolas Garcia 3ºC</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="list">
+                <h2 class="tit" >BackEnd</h2>
+                <div class="cols">
+                    <ul>
+                        <li> Danilo Keithi 3ºC</li>
+                        <li>Danilo Correia 3ºC</li>
+                        <li>David Santos 3ºC</li>
+                        <li>Enzo Siqueira 3ºC</li>
+                        <li>Gabriel Dantas 3ºC</li>
+                        <li>Gabriel Garcia 3ºC</li>
+                        <li>Nicolas Garcia 3ºC</li>
+                    </ul>
+                    <ul>
+                        <li> Danilo Keithi 3ºC</li>
+                        <li>Danilo Correia 3ºC</li>
+                        <li>David Santos 3ºC</li>
+                        <li>Enzo Siqueira 3ºC</li>
+                        <li>Gabriel Dantas 3ºC</li>
+                        <li>Gabriel Garcia 3ºC</li>
+                        <li>Nicolas Garcia 3ºC</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <!-- FIM CONTEÚDO -->
+         
+        <!-- FOOTER -->
+
+        <!-- FIM FOOTER -->
+
+        <!-- !AQUI VAI O JS DA UA PÁGINA (CASO TENHA)! -->
+        <script src=""></script>
+    </body>
+</html>
+
+<!-- !LEMBRE-SE DE SEMPRE VISUALIZAR EM UMA TELA RESPONSIVA -->

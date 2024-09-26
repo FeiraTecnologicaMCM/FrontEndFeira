6ab14ce7b96c519a8f48a15af91c7eb6fcbd5894
FeiraTecnologicaMCM <appfeira3c2024@gmail.com>
2024-09-26 10:48:51 -0300

    Add files via upload

---
+++ app/public/src/infoprojetos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Info - Projetos</title>
+        <?php
+            include('../assets/components/head.php')
+        ?>
+    <body>
+        <?php
+            include('../assets/components/header.php')
+         ?>
+
+         <div class="ProjetosdafeiraContainer">
+             <h1 class="ProjetosdafeiraH1">Mapeamento da feira tecnológica</h1>
+</div>
+     
+    
+</body>
+</html>

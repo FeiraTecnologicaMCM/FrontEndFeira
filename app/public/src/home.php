15dfb112a61dea1e63612c547b7840881fe7aa72
FeiraTecnologicaMCM <appfeira3c2024@gmail.com>
2024-10-03 12:43:09 -0300

    Add files via upload

--- app/public/src/home.php
+++ app/public/src/home.php
@@ -1,40 +1,49 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Feira Tecnológica 2024</title>
+  <?php
+    include('../assets/components/head.php')
+  ?>
   <link rel="stylesheet" href="../assets/css/home.css">
 </head>
 <body>
 
-  <header>
-    <div class="logo">
-      <img src="../assets/img/etecmcm.png" alt="Logo ETEC Maria Cristina Medeiros">
-    </div>
-  </header>
-
+  <?php
+    include('../assets/components/header.php')
+  ?>
   <main>
-    <section class="evento">
-      <h1>FEIRA TECNOLÓGICA 2024</h1>
-      <h2>ETEC Maria Cristina Medeiros</h2>
-    </section>
-
     <section class="projetos">
       <div class="projetos-header">
         <h3>Projetos</h3>
         <button id="verTodos">Ver todos</button>
       </div>
       
-      <div class="slider-container">
-        <div class="slider" id="slider">
-          <div class="slide"></div>
-          <div class="slide"></div>
-          <div class="slide"></div>
+      <div class='container-slider'>
+        <div class="slider-container">
+          <div class="slider" id="slider">
+            <div class="slide"></div>
+            <div class="slide"></div>
+            <div class="slide"></div>
+          </div>
         </div>
         <button class="slider-btn" id="prevBtn">&lt;</button>
         <button class="slider-btn" id="nextBtn">&gt;</button>
       </div>
+      
+    </section>
+    <section class='mapeamento'>
+      <div class="projetos-header">
+        <h3>Mapeamento</h3>
+        <button id="verTodos">Ver mapa</button>
+      </div>
+      <img class='img-mapa' src="../assets/img/mapa.png" alt="">
+    </section>
+    <section class='ods'>
+      <div class="projetos-header">
+        <h3>ODS's</h3>
+        <button id="verTodos">Ver todos</button>
+      </div>
+
     </section>
   </main>
 

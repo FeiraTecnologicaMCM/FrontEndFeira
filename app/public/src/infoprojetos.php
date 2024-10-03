767cb1b0db3934385ccd469c0043635779f8a6d3
FeiraTecnologicaMCM <appfeira3c2024@gmail.com>
2024-10-02 21:01:56 -0300

    Add files via upload

--- app/public/src/infoprojetos.php
+++ app/public/src/infoprojetos.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Info - Projetos</title>
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/infoprojetos.css">
         <?php
             include('../assets/components/head.php')
         ?>
@@ -13,9 +15,33 @@
             include('../assets/components/header.php')
          ?>
 
-         <div class="ProjetosdafeiraContainer">
-             <h1 class="ProjetosdafeiraH1">Mapeamento da feira tecnológica</h1>
-</div>
+        <div class="infoContainer">
+            <h1> Projetos da feira tecnológica</h1>
+            <h1 class='Quadros'> Quadro n°1 </h1>
+
+            <img class ='fotoinfo'src="../assets/img/fotoinfo.png" alt="">
+
+            <h2> Projeto PERR - 3C</h2>
+
+            <p class='txt'>  O projeto PERR é focado no desenvolvimento de um site de apoio a Imigrantes e Refugiados no mercado de trabalho. Muitas dessas pessoas chegam ao Brasil e possuem dificuldade ao encontrar um emprego. O PERR veio para facilitar esse processo.</p>
+
+            <h2 class='odsutilizadas'> Ods's Utilizadas</h2>
+        
+            <div class='Odss'>
+                <img class ='fotods'src="../assets/img/ods1.png" alt="">
+                <img class ='fotods'src="../assets/img/ods2.png" alt="">
+                <img class ='fotods'src="../assets/img/ods3.png" alt="">
+            </div>
+
+            <h2 class='integrantes'> Integrantes</h2>
+
+            <p class='txt'> Thales Magno, Memphis Depay, Rogrigo Garro, Cristiano Romero, Luciano Reis</p>
+
+            <a class="btnVoto" href="">VOTAR COMO MELHOR PROJETO</a>
+
+            <img class ='fotocps'src="../assets/img/logocps.png" alt="">
+         </div>
+
      
     
 </body>
